f4cd423fdae706c10117cf89ce19e2720feb4640
Alexander <taxidermyst@gmail.com>
2023-01-20 16:47:51 +0300

    f-strings added

--- ProgCheckout.py
+++ ProgCheckout.py
@@ -18,12 +18,25 @@ def search_and_check(file_to_open, prg_name, prg_time, blc_time, blc_name):
                 # print(line_block_params[74:81])
                 if prg_name[51:53] == prg_time and line_block_params[16:20] == blc_time \
                         and line_block_params[74:81] == blc_name:
-                    print("Программа стоит в правильном блоке")
+                    print(f"Программа {line[13:32]} стоит в блоке {line_block_params[74:81]} {line_block_params[16:20]}")
                 else:
                     print(
-                        "Программа стоит в неправильном временном блоке или в блоке предназначенном для другой программы")
-                line_index.append(log.index(line))
+                        f"Программа {line[13:32]} стоит в неправильном временном блоке "
+                        "или в блоке предназначенном для другой программы")
+                    line_index.append(log.index(line))
+            # else:
+            #     print("Программа с таким именем файла не найдена")
 
-search_and_check("EDIT_LOG.txt", '\s\s\S\S\S\sName\s=\s\S\S\SNEWS\s\d\d\s\d\d\s\d\d\s08', "08", "8:00", "Новости")
+
+
+
+file_to_open = "EDIT_LOG.txt"
+prg_name = '\s\s\S\S\S\sName\s=\s\S\S\SNEWS\s\d\d\s\d\d\s\d\d\s08'
+# prg_name = '[Н]NEWS 02 12 22 08'
+prg_time = "08"
+blc_time = "8:00"
+blc_name = "Новости"
+# search_and_check("EDIT_LOG.txt", '\s\s\S\S\S\sName\s=\s\S\S\SNEWS\s\d\d\s\d\d\s\d\d\s08', "08", "8:00", "Новости")
+search_and_check(file_to_open, prg_name, prg_time, blc_time, blc_name)
 
 
