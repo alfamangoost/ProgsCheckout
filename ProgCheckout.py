d38734c1922fac6f41a99ed403b3b8b1b37236cf
Alexander <taxidermyst@gmail.com>
2023-01-20 16:01:34 +0300

    made function from circle

--- ProgCheckout.py
+++ ProgCheckout.py
@@ -2,7 +2,7 @@ import re
 
 line_index = []
 
-def search_and_check(file_to_open, prg_name, prg_time, blc_time):
+def search_and_check(file_to_open, prg_name, prg_time, blc_time, blc_name):
 
     with open(file_to_open, 'r') as log_file:
         log = log_file.readlines()
@@ -10,19 +10,20 @@ def search_and_check(file_to_open, prg_name, prg_time, blc_time):
             # print(line)
             if re.search(prg_name, line) != None:
                 # print(line)
-                index_of_block_time = log.index(line) - 7
-                block_time = log[index_of_block_time]
-                print(block_time)
-                print(prg_name[51:53])
-                print(block_time[16:20])
-                print(block_time[74:81])
-                if prg_name[51:53] == prg_time and block_time[16:20] == blc_time and block_time[74:81] == "Новости":
+                index_of_line_block_params = log.index(line) - 7
+                line_block_params = log[index_of_line_block_params]
+                # print(line_block_params)
+                # print(prg_name[51:53])
+                # print(line_block_params[16:20])
+                # print(line_block_params[74:81])
+                if prg_name[51:53] == prg_time and line_block_params[16:20] == blc_time \
+                        and line_block_params[74:81] == blc_name:
                     print("Программа стоит в правильном блоке")
                 else:
                     print(
                         "Программа стоит в неправильном временном блоке или в блоке предназначенном для другой программы")
                 line_index.append(log.index(line))
 
-search_and_check("EDIT_LOG.txt", '\s\s\S\S\S\sName\s=\s\S\S\SNEWS\s\d\d\s\d\d\s\d\d\s08', "08", "8:00" )
+search_and_check("EDIT_LOG.txt", '\s\s\S\S\S\sName\s=\s\S\S\SNEWS\s\d\d\s\d\d\s\d\d\s08', "08", "8:00", "Новости")
 
 
