2dea7aa7ebd55a28a180c7493e03a30cfe2292ef
Alexander <taxidermyst@gmail.com>
2023-01-20 15:43:02 +0300

    made function from circle

--- ProgCheckout.py
+++ ProgCheckout.py
@@ -1,27 +1,28 @@
 import re
 
-prg_name = '\s\s\S\S\S\sName\s=\s\S\S\SNEWS\s\d\d\s\d\d\s\d\d\s08'
 line_index = []
-# with open("test_log.txt", 'r') as log_file:
-with open("EDIT_LOG.txt", 'r') as log_file:
-    log = log_file.readlines()
-    for line in log:
-        # print(line)
-        if re.search(prg_name, line) != None:
-            #print(line)
-            index_of_block_time = log.index(line) - 7
-            block_time = log[index_of_block_time]
-            print(block_time)
-            print(prg_name[51:53])
-            print(block_time[16:20])
-            print(block_time[74:81])
-            if prg_name[51:53] == "08" and block_time[16:20] == "8:00" and block_time[74:81] == "Новости":
-                print("Программа стоит в правильном блоке")
-            else:
-                print("Программа стоит в неправильном временном блоке или в блоке предназначенном для другой программы")
-            line_index.append(log.index(line))
 
+def search_and_check(file_to_open, prg_name):
 
-# print(log)
-print(line_index)
+    with open(file_to_open, 'r') as log_file:
+        log = log_file.readlines()
+        for line in log:
+            # print(line)
+            if re.search(prg_name, line) != None:
+                # print(line)
+                index_of_block_time = log.index(line) - 7
+                block_time = log[index_of_block_time]
+                print(block_time)
+                print(prg_name[51:53])
+                print(block_time[16:20])
+                print(block_time[74:81])
+                if prg_name[51:53] == "08" and block_time[16:20] == "8:00" and block_time[74:81] == "Новости":
+                    print("Программа стоит в правильном блоке")
+                else:
+                    print(
+                        "Программа стоит в неправильном временном блоке или в блоке предназначенном для другой программы")
+                line_index.append(log.index(line))
+
+search_and_check("EDIT_LOG.txt", '\s\s\S\S\S\sName\s=\s\S\S\SNEWS\s\d\d\s\d\d\s\d\d\s08')
+
 
