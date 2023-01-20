8c465fe120aa333a58b14922d8455f44c2af0185
Alexander <taxidermyst@gmail.com>
2023-01-20 13:41:08 +0300

    split log in lines and search index of line with name of program

--- ProgCheckout.py
+++ ProgCheckout.py
@@ -1,1 +1,15 @@
-print("Hello World!!!")
+import re
+
+news = '\s\s\S\S\S\sName\s=\s\S\S\SNEWS\s\d\d\s\d\d\s\d\d\s\d\d'
+line_index = []
+# with open("test_log.txt", 'r') as log_file:
+with open("EDIT_LOG.txt", 'r') as log_file:
+    log = log_file.readlines()
+    for line in log:
+        # print(line)
+        if re.search(news, line) != None:
+            line_index.append(log.index(line))
+
+# print(log)
+print(line_index)
+
